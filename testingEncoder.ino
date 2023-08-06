950232d3fe5eae5dad680f9c4dd9fc02c8a43d44
sdpb21 <jytprf@gmail.com>
2023-08-06 16:50:07 -0300

    start

---
+++ testingEncoder.ino
@@ -0,0 +1,16 @@
+byte duty=217, i;
+volatile long contador=0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
